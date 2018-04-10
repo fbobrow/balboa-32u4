ff9a2090a39925865184c5316de950c9abf32c1a
Fabio Bobrow <fbob@me.com>
2018-04-10 18:41:36 -0300

    Added IMU library

---
+++ IMU.cpp
@@ -0,0 +1,6 @@
+#include "Arduino.h"
+#include "IMU.h"
+
+IMU::IMU()
+{
+}
---
+++ IMU.h
@@ -0,0 +1,13 @@
+#ifndef IMU_h
+#define IMU_h
+
+#include "Arduino.h"
+
+class IMU
+{
+  public:
+    // Initialize class
+    IMU();
+};
+
+#endif
