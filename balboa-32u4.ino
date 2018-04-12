047e2d39a0299481d1e058eedaff261e581e0250
Fabio Bobrow <fbob@me.com>
2018-04-12 14:22:30 -0300

    Updated IMU library

--- balboa-32u4.ino
+++ balboa-32u4.ino
@@ -1,18 +1,30 @@
 #include "src/drivers/Motors.h"
-//#include "src/drivers/IMU.h"
+#include "src/drivers/IMU.h"
 
 Motors motors;
-//IMU imu;
+IMU imu;
 
 void setup()
 {
-  Serial.begin(9600);
+  imu.init();
 }
 
 void loop()
 {
   //motors.set_voltages(2,2);
-  //imu.read_accel();
-  Serial.println("Hello world!");
+  imu.read();
+  Serial.print("X: ");
+  Serial.print(imu.accel.x);
+  Serial.print("  |  Y: ");
+  Serial.print(imu.accel.y);
+  Serial.print("  |  Z: ");
+  Serial.println(imu.accel.z);  
+  Serial.print("X: ");
+  Serial.print(imu.gyro.x);
+  Serial.print("  |  Y: ");
+  Serial.print(imu.gyro.y);
+  Serial.print("  |  Z: ");
+  Serial.println(imu.gyro.z);
+  Serial.println("");
   delay(1000);
 }
--- src/drivers/IMU.cpp
+++ src/drivers/IMU.cpp
@@ -2,7 +2,7 @@
 #include "Arduino.h"
 #include "Wire.h"
 
-#define address 0b1101011
+#define ADDRESS 0x6B
 
 IMU::IMU()
 {
@@ -14,9 +14,20 @@ IMU::IMU()
   accel.z = 0;
 }
 
+void IMU::init()
+{
+  //
+  Wire.begin();
+  // ODR = 1000 (1.66 kHz (high performance)); FS_XL = 00 (+/-2 g full scale)
+  write_register(CTRL1_XL,0b10000000);
+  // ODR = 1000 (1.66 kHz (high performance)); FS_G = 00 (+/-245 dps)
+  write_register(CTRL2_G,0b10000000);
+}
+
 void IMU::write_register(uint8_t reg, uint8_t data)
 {
-  Wire.beginTransmission(address);
+  //
+  Wire.beginTransmission(ADDRESS);
   Wire.write(reg);
   Wire.write(data);
   Wire.endTransmission();
@@ -24,29 +35,59 @@ void IMU::write_register(uint8_t reg, uint8_t data)
 
 uint8_t IMU::read_register(uint8_t reg)
 {
+  //
   uint8_t value;
-
-  Wire.beginTransmission(address);
+  //
+  Wire.beginTransmission(ADDRESS);
   Wire.write(reg);
   Wire.endTransmission();
-  Wire.requestFrom(address, (uint8_t)1);
+  Wire.requestFrom(ADDRESS,1);
   value = Wire.read();
   Wire.endTransmission();
-
+  //
   return value;
 }
 
 void IMU::read_accel()
 {
-uint8_t xla = read_register(OUTX_L_XL);
-uint8_t xha = read_register(OUTX_H_XL);
-uint8_t yla = read_register(OUTY_L_XL);
-uint8_t yha = read_register(OUTY_H_XL);
-uint8_t zla = read_register(OUTZ_L_XL);
-uint8_t zha = read_register(OUTZ_H_XL);
+  // Read two bytes (high and low) of accelerometer data (2 x 8 bit for each axis)
+  uint8_t x_raw_l = read_register(OUTX_L_XL);
+  uint8_t x_raw_h = read_register(OUTX_H_XL);
+  uint8_t y_raw_l = read_register(OUTY_L_XL);
+  uint8_t y_raw_h = read_register(OUTY_H_XL);
+  uint8_t z_raw_l = read_register(OUTZ_L_XL);
+  uint8_t z_raw_h = read_register(OUTZ_H_XL);
+  // Combine high and low bytes into a single accelerometer data (16 bit for each axis)
+  accel.x_raw = (int16_t)(x_raw_h << 8 | x_raw_l);
+  accel.y_raw = (int16_t)(y_raw_h << 8 | y_raw_l);
+  accel.z_raw = (int16_t)(z_raw_h << 8 | z_raw_l);
+  // Convert raw data into SI units (m/s^2)
+  accel.x = accel.x_raw*(0.061/1000.0)*(9.80665);
+  accel.y = accel.y_raw*(0.061/1000.0)*(9.80665);
+  accel.z = accel.z_raw*(0.061/1000.0)*(9.80665);
+}
 
-// combine high and low bytes
-accel.x_raw = (int16_t)(xha << 8 | xla);
-accel.y_raw = (int16_t)(yha << 8 | yla);
-accel.z_raw = (int16_t)(zha << 8 | zla);
+void IMU::read_gyro()
+{
+  // Read two bytes (high and low) of accelerometer data (2 x 8 bit for each axis)
+  uint8_t x_raw_l = read_register(OUTX_L_G);
+  uint8_t x_raw_h = read_register(OUTX_H_G);
+  uint8_t y_raw_l = read_register(OUTY_L_G);
+  uint8_t y_raw_h = read_register(OUTY_H_G);
+  uint8_t z_raw_l = read_register(OUTZ_L_G);
+  uint8_t z_raw_h = read_register(OUTZ_H_G);
+  // Combine high and low bytes into a single accelerometer data (16 bit for each axis)
+  gyro.x_raw = (int16_t)(x_raw_h << 8 | x_raw_l);
+  gyro.y_raw = (int16_t)(y_raw_h << 8 | y_raw_l);
+  gyro.z_raw = (int16_t)(z_raw_h << 8 | z_raw_l);
+  // Convert raw data into SI units (m/s^2)
+  gyro.x = gyro.x_raw*(4.375/1000.0)*(3.1415/180);
+  gyro.y = gyro.y_raw*(4.375/1000.0)*(3.1415/180);
+  gyro.z = gyro.z_raw*(4.375/1000.0)*(3.1415/180);
+}
+
+void IMU::read()
+{
+  read_accel();
+  read_gyro();
 }
